36166dcee8134ac850dd3b2d3854f797896604c3
Kentaro Masukawa <masukawa.kentaro@classmethod.jp>
2025-04-20 16:58:53 +0900

    CloudFront用ACMの追加

--- terraform/certificate.tf
+++ terraform/certificate.tf
@@ -16,3 +16,22 @@ module "alb_acm" {
     Name = "${local.alb_sub_domain_name}.${local.hosted_zone_name}"
   }
 }
+
+module "cloudfront_acm" {
+  providers = {
+    aws = aws.us-east-1
+  }
+
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 5.1.1"
+
+  domain_name = "${local.cloudfront_sub_domain_name}.${local.hosted_zone_name}"
+  zone_id     = data.aws_route53_zone.hostzone.zone_id
+
+  validation_method   = "DNS"
+  wait_for_validation = true
+
+  tags = {
+    Name = "${local.cloudfront_sub_domain_name}.${local.hosted_zone_name}"
+  }
+}
